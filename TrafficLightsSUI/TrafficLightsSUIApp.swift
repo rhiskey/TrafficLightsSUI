64ef3754e21101068ac2314e78af37b51137839a
rhiskey <first3395@gmail.com>
2022-04-28 09:49:46 +0300

    Initial Commit

---
+++ TrafficLightsSUI/TrafficLightsSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsSUIApp.swift
+//  TrafficLightsSUI
+//
+//  Created by Владимир Киселев on 28.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
